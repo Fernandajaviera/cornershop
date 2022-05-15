4b0d327898d7ef2577e48828a5ec2158f27e64b6
Fernandajaviera <fernandajaviera64@gmail.com>
2022-05-15 12:01:33 -0400

    Creacion de ordenes en seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,25 @@
 
 repartidor = User.create!(email: "repartidor@cornershop.cl", password: "123456", password_confirmation: "123456", role: "worker")
 cliente = User.create!(email: "cliente@cornershop.cl", password: "123456", password_confirmation: "123456")
+
+
+5.times do
+    nueva_orden = Order.new(
+        store_name: Faker::Company.name, 
+        dispatch_date: rand(1.months).seconds.ago,
+        dispatch_price: (10 + rand() * 50).to_i
+    )
+
+    3.times do
+        nuevo_item = Item.create!(
+            name: [Faker::Food.fruits, Faker::Food.vegetables, Faker::Food.ingredient].sample,
+            stock: (rand() * 10).to_i,
+            price:  (10 + rand() * 50).to_i,
+            available: [true, false].sample
+        )
+        nueva_orden.items << nuevo_item
+    end
+
+    cliente.orders << nueva_orden
+    repartidor.orders << nueva_orden
+end
